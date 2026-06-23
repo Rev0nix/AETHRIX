36ac363bc28548fa7f1dc604126e232433bdecd8
Revanth HM <revanthhmgowda@gmail.com>
2026-06-23 13:36:42 +0530

    Update contact information

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -75,7 +75,7 @@ const Contact = () => {
             className="inline-flex items-center gap-2 text-sm border-b border-white/20 hover:border-white pb-1 mb-10"
           >
             <TbMail />
-            <aethrixofficial className="inline-block">aethrixofficial</aethrixofficial>@gmail.com
+            aethrixofficial.in@gmail.com
           </a>
 
           {/* Support */}
